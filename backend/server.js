b77f546a020644830901cfb114466df64ab76dd4
pkarisa <faith.pendo@student.moringaschool.com>
2023-04-04 21:51:20 +0300

    local host changed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url =  'mongodb+srv://fkarisa:K01naImv2a9D8FY2@cluster0.czitiqk.mongodb.net/?retryWrites=true';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
